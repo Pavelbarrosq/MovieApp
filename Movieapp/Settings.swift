6e706871f12ae2f56d43c9bf326fb1f4546551f1
Avecca <hanna_astlind@hotmail.com>
2019-05-16 15:03:01 +0200

    organized code, nulling the searchbar on genre switch, bugfixed scrolling to top when empty bug

---
+++ Movieapp/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Movieapp
+//
+//  Created by Hanna Astlind on 2019-05-16.
+//  Copyright © 2019 Pavel Barros Quintanilla. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    
+    let moviePath = "https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.asc&api_key=d5c04206ed27091dae4a910d147726cc&vote_count.gte=50&page="
+    let showPath =  "https://api.themoviedb.org/3/discover/tv?sort_by=vote_average.asc&api_key=d5c04206ed27091dae4a910d147726cc&vote_count.gte=50&page="
+    
+    
+    
+}
